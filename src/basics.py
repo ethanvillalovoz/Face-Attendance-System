ce630d751b31312df33e9ea4b223fef3f15702df
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-08 00:57:51 -0700

    Implement face detection and display for Elon Musk images using face_recognition and OpenCV

--- src/basics.py
+++ src/basics.py
@@ -8,6 +8,22 @@ img_Elon_Musk = cv2.cvtColor(img_Elon_Musk, cv2.COLOR_BGR2RGB)
 img_Elon_Musk_test = face_recognition.load_image_file("image_basics_data/Elon_Musk_Test.jpeg")
 img_Elon_Musk_test = cv2.cvtColor(img_Elon_Musk_test, cv2.COLOR_BGR2RGB)
 
+face_locations = face_recognition.face_locations(img_Elon_Musk)[0]
+face_encodings = face_recognition.face_encodings(img_Elon_Musk)[0]
+cv2.rectangle(img_Elon_Musk, 
+              (face_locations[3], face_locations[0]), 
+              (face_locations[1], face_locations[2]), 
+              (255, 0, 255), 
+              2)
+
+face_locations_test = face_recognition.face_locations(img_Elon_Musk_test)[0]
+face_encodings_test = face_recognition.face_encodings(img_Elon_Musk_test)[0]
+cv2.rectangle(img_Elon_Musk_test, 
+              (face_locations_test[3], face_locations_test[0]), 
+              (face_locations_test[1], face_locations_test[2]), 
+              (255, 0, 255), 
+              2)
+
 cv2.imshow("Elon Musk", img_Elon_Musk)
 cv2.imshow("Elon Musk Test", img_Elon_Musk_test)
 cv2.waitKey(0)
