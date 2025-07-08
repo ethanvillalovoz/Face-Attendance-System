5d46808a7958505501115ca81e751d7e35ade890
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-08 01:30:13 -0700

    Implement image loading, class name extraction, and face encoding for attendance

--- src/AttendanceProject.py
+++ src/AttendanceProject.py
@@ -13,3 +13,14 @@ for cl in my_list:
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print("Encoding Complete")
