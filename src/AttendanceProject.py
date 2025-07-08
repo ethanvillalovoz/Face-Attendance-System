8fbcbc43c4b6a62f12b01b9145b53ff2d1a3d157
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-08 01:27:59 -0700

    Add initial image loading and class name extraction for attendance project

--- src/AttendanceProject.py
+++ src/AttendanceProject.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
 import face_recognition
+import os
+
+path = "data/image_attendance_data"
+images = []
+classNames = []
+my_list = os.listdir(path)
+print(my_list)
+for cl in my_list:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
