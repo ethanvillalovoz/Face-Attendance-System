3cc6938ea60efee278db60c52beb7415ddbec145
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-11 23:48:48 -0700

    CI/CD v2

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders FaceTrack header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/FaceTrack/i);
+  expect(headerElement).toBeInTheDocument();
 });
